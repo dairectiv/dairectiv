56b52558ffe315052b9cb8c497b50e0c506de6b8
Thomas Boileau (toham) <thomas.boileau@yousign.com>
2025-12-25 13:06:45 +0100

    fix(dai-21): add exception unwrapping to QueryBus for consistency with CommandBus

--- api/tests/Integration/SharedKernel/Infrastructure/Symfony/Messenger/Bus/MessengerQueryBusTest.php
+++ api/tests/Integration/SharedKernel/Infrastructure/Symfony/Messenger/Bus/MessengerQueryBusTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Dairectiv\Tests\Integration\SharedKernel\Infrastructure\Symfony\Messenger\Bus;
 
+use Dairectiv\Tests\Fixtures\ErrorQuery;
 use Dairectiv\Tests\Fixtures\Query;
 use Dairectiv\Tests\Framework\IntegrationTestCase;
 
@@ -13,5 +14,11 @@ final class MessengerQueryBusTest extends IntegrationTestCase
     {
         $output = $this->fetch(new Query\Input('foo'));
         self::assertEquals(new Query\Output('foo'), $output);
+    }
+
+    public function testItShouldThrowException(): void
+    {
+        $this->expectExceptionObject(new \RuntimeException('This query is not supported yet'));
+        $this->fetch(new ErrorQuery\Input('foo'));
     }
 }
