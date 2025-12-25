5cf7716c4c3b20093840d9ba37f0d4c8b5cc33e6
Thomas Boileau (toham) <thomas.boileau@yousign.com>
2025-12-25 12:44:29 +0100

    test(dai-23): add unit tests for DirectiveName value object with validation

--- api/src/Authoring/Domain/Directive/DirectiveName.php
+++ api/src/Authoring/Domain/Directive/DirectiveName.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dairectiv\Authoring\Domain\Directive;
 
+use Dairectiv\SharedKernel\Domain\Assert;
+
 final readonly class DirectiveName implements \Stringable
 {
     private function __construct(public string $name)
@@ -12,6 +14,9 @@ final readonly class DirectiveName implements \Stringable
 
     public static function fromString(string $name): self
     {
+        Assert::notEmpty($name, 'Directive name cannot be empty.');
+        Assert::maxLength($name, 255, 'Directive name is too long.');
+
         return new self($name);
     }
 
