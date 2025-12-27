ae39164f8d3b511d0cf0ba36e5f4b19ce67ebfd7
Thomas Boileau (toham) <thomas.boileau@yousign.com>
2025-12-27 19:02:29 +0100

    test(dai-45): add integration tests for Publish Directive

---
+++ api/tests/Integration/Authoring/Application/Directive/PublishTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dairectiv\Tests\Integration\Authoring\Application\Directive;
+
+use Dairectiv\Authoring\Application\Directive\Publish\Input;
+use Dairectiv\Authoring\Domain\Object\Directive\DirectiveId;
+use Dairectiv\Authoring\Domain\Object\Directive\DirectiveState;
+use Dairectiv\Authoring\Domain\Object\Directive\Event\DirectivePublished;
+use Dairectiv\Authoring\Domain\Object\Directive\Exception\DirectiveNotFoundException;
+use Dairectiv\Authoring\Domain\Object\Rule\Rule;
+use Dairectiv\Authoring\Domain\Object\Skill\Skill;
+use Dairectiv\Authoring\Infrastructure\Zenstruck\Foundry\Factory\Rule\RuleFactory;
+use Dairectiv\Authoring\Infrastructure\Zenstruck\Foundry\Factory\Skill\SkillFactory;
+use Dairectiv\Tests\Framework\IntegrationTestCase;
+use Zenstruck\Foundry\Test\Factories;
+
+final class PublishTest extends IntegrationTestCase
+{
+    use Factories;
+
+    public function testItShouldPublishRule(): void
+    {
+        $rule = RuleFactory::new()->withId('rule-to-publish')->create();
+
+        $this->execute(new Input($rule->id));
+
+        self::assertDomainEventHasBeenDispatched(DirectivePublished::class);
+        $rule = $this->findEntity(Rule::class, ['id' => DirectiveId::fromString($rule->id)], true);
+
+        self::assertSame(DirectiveState::Published, $rule->state);
+    }
+
+    public function testItShouldPublishSkill(): void
+    {
+        $skill = SkillFactory::createOne();
+
+        $this->execute(new Input($skill->id));
+
+        self::assertDomainEventHasBeenDispatched(DirectivePublished::class);
+        $skill = $this->findEntity(Skill::class, ['id' => DirectiveId::fromString($skill->id)], true);
+
+        self::assertSame(DirectiveState::Published, $skill->state);
+    }
+
+    public function testItShouldThrowExceptionWhenDirectiveNotFound(): void
+    {
+        $this->expectException(DirectiveNotFoundException::class);
+
+        $this->execute(new Input('non-existent-directive'));
+    }
+}
