8b59f6fcbe216d81b936b440f488dc95f6b9cc03
Thomas Boileau (toham) <thomas.boileau@yousign.com>
2025-12-30 16:52:52 +0100

    feat(dai-73): implement update rule example use case

---
+++ api/src/Authoring/Application/Rule/UpdateExample/Handler.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dairectiv\Authoring\Application\Rule\UpdateExample;
+
+use Dairectiv\Authoring\Domain\Object\Directive\DirectiveId;
+use Dairectiv\Authoring\Domain\Object\Rule\Example\ExampleId;
+use Dairectiv\Authoring\Domain\Object\Rule\Rule;
+use Dairectiv\Authoring\Domain\Repository\DirectiveRepository;
+use Dairectiv\SharedKernel\Application\Command\CommandHandler;
+use Dairectiv\SharedKernel\Domain\Object\Assert;
+
+final readonly class Handler implements CommandHandler
+{
+    public function __construct(private DirectiveRepository $directiveRepository)
+    {
+    }
+
+    public function __invoke(Input $input): void
+    {
+        Assert::true(
+            null !== $input->good || null !== $input->bad || null !== $input->explanation,
+            'At least one field must be provided.'
+        );
+
+        $ruleId = DirectiveId::fromString($input->ruleId);
+        $rule = $this->directiveRepository->getDirectiveById($ruleId);
+
+        \assert($rule instanceof Rule);
+
+        $exampleId = ExampleId::fromString($input->exampleId);
+        $example = $rule->examples->filter(
+            static fn ($e) => $e->id->equals($exampleId)
+        )->first();
+
+        Assert::notFalse($example, \sprintf('Example with ID "%s" not found.', $input->exampleId));
+
+        $example->update(
+            $input->good,
+            $input->bad,
+            $input->explanation,
+        );
+
+        $this->directiveRepository->save($rule);
+    }
+}
