f2a323e7270adc91cd7b5426900f94d51202f52a
Thomas Boileau (toham) <thomas.boileau@yousign.com>
2025-12-30 16:52:52 +0100

    feat(dai-73): implement update rule example use case

---
+++ api/src/Authoring/Application/Rule/UpdateExample/Input.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dairectiv\Authoring\Application\Rule\UpdateExample;
+
+use Dairectiv\SharedKernel\Application\Command\Command;
+
+final readonly class Input implements Command
+{
+    public function __construct(
+        public string $ruleId,
+        public string $exampleId,
+        public ?string $good = null,
+        public ?string $bad = null,
+        public ?string $explanation = null,
+    ) {
+    }
+}
