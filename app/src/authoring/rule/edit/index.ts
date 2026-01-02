c5b0128e0538fd70442f9684914f3a84a1550069
Thomas Boileau (toham) <thomas.boileau@yousign.com>
2026-01-02 21:30:29 +0100

    feat(dai-155): integrate examples manager in rule edit page
    Add RuleExamplesManager section below the metadata form on
    the rule edit page. Update barrel exports to expose all new
    components, hooks, and their types.

--- app/src/authoring/rule/edit/index.ts
+++ app/src/authoring/rule/edit/index.ts
@@ -4,9 +4,31 @@ export {
   type EditRuleFormProps,
   type EditRuleFormValues,
 } from "./components/edit-rule-form";
+export { RuleExampleCard, type RuleExampleCardProps } from "./components/rule-example-card";
+export {
+  RuleExampleForm,
+  type RuleExampleFormProps,
+  type RuleExampleFormValues,
+} from "./components/rule-example-form";
+export {
+  RuleExamplesManager,
+  type RuleExamplesManagerProps,
+} from "./components/rule-examples-manager";
 
 // Hooks
+export {
+  type UseAddRuleExampleOptions,
+  useAddRuleExample,
+} from "./hooks/use-add-rule-example";
+export {
+  type UseRemoveRuleExampleOptions,
+  useRemoveRuleExample,
+} from "./hooks/use-remove-rule-example";
 export { type UseUpdateRuleOptions, useUpdateRule } from "./hooks/use-update-rule";
+export {
+  type UseUpdateRuleExampleOptions,
+  useUpdateRuleExample,
+} from "./hooks/use-update-rule-example";
 
 // Pages
 export { EditRulePage } from "./pages/edit-rule.page";
